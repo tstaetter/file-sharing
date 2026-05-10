dbaeb8ccb8c962b03629ba9ef265f4db1bc6cdac
tstaetter <thomas.staetter@gmail.com>
2026-05-10 18:46:46 +0200

    fix: update svelte.config

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,14 +1,10 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-	},
+	preprocess: vitePreprocess(),
 	kit: {
-		// adapter-node produces a self-contained Node server for production deployment.
-		// See https://svelte.dev/docs/kit/adapter-node for configuration options.
 		adapter: adapter()
 	}
 };
